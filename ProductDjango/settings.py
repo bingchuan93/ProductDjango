50455206d0585390c9cb2813631b40ca7d4a2829
Bing Chuan <bingchuan93@gmail.com>
2018-12-29 21:01:10 +0800

    Restored back to original project :)

--- ProductDjango/settings.py
+++ ProductDjango/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages',
+    'products',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- ProductDjango/urls.py
+++ ProductDjango/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pages.views import home_view, contact_view, about_view, products_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+    path('contact/', contact_view, name='contact'),
+    path('about/', about_view, name='about'),
+    path('products/', products_view, name='products'),
 ]
