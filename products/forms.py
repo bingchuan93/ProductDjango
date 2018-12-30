3037196b5386106434072eea727f4a04d32dc254
Bing Chuan <bingchuan93@gmail.com>
2018-12-30 21:51:09 +0800

    End of Tutorial 28

--- products/forms.py
+++ products/forms.py
@@ -3,8 +3,45 @@ from django import forms
 from .models import Product
 
 class ProductCreateForm(forms.ModelForm):
+    title           = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder' : "Enter product name"
+            }
+        )
+    )
+    price           = forms.DecimalField(initial=1.99)
     class Meta:
         model = Product
         fields = [
-            'title', 'description', 'price'
+            "title", "description", "price"
         ]
+    
+    # def clean_title(self, *args, **kwargs):
+    #     title = self.cleaned_data.get("title")
+    #     if not "CFE" in title:
+    #         raise forms.ValidationError("This is not a valid title")
+    #     else:
+    #         return title
+
+
+class RawProductForm(forms.Form):
+    title           = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'placeholder' : "Enter product name"
+            }
+        )
+    )
+    description     = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'placeholder': "Enter a description",
+                'class' : "materialize-textarea",
+                'id' : "id",
+                'rows' : 50
+            }
+        )
+    )
+    price           = forms.DecimalField(initial=1.99)
+    featured        = forms.BooleanField()
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from .models import Product
-from .forms import ProductCreateForm
+from .forms import ProductCreateForm, RawProductForm
 # Create your views here.
 def products_view(request, *args, **kwargs):
     context = {}
@@ -23,26 +23,45 @@ def product_details_view(request):
 
     return render(request, "products/product_details.html", context)
 
-# def product_create_view(request):
-#     form = ProductCreateForm(request.POST or None)
+def product_create_view(request): #Using built in django 'automated' forms
+    initial_data = {
+        'title': "This is an initial product name"
+    }
+    obj = Product.objects.get(id=1)
+    form = ProductCreateForm(request.POST or None, instance=obj) #changes database data
 
-#     if form.is_valid():
-#         form.save()
-#         form = ProductCreateForm()
+    if form.is_valid():
+        form.save()
+        form = ProductCreateForm()
 
+    context = {
+        'form': form
+    }
+
+    return render(request, "products/product_create.html", context)
+
+# def product_create_view(request):   #raw HTML Form Example
+#     print(request.GET)
+#     print(request.POST)
+#     if request.method == 'POST':
+#         title = request.POST.get('title')
+#         print(title)
+#     context = {
+#     }
+
+#     return render(request, "products/product_create.html", context)
+
+# def product_create_view(request):   #pure HTML Form Example
+#     form = RawProductForm()
+#     if request.method == 'POST':
+#         form = RawProductForm(request.POST)
+#         if form.is_valid():
+#             print(form.cleaned_data)
+#             Product.objects.create(**form.cleaned_data)
+#         else:
+#             print(form.errors)
 #     context = {
 #         'form': form
 #     }
 
 #     return render(request, "products/product_create.html", context)
-
-def product_create_view(request):
-    print(request.GET)
-    print(request.POST)
-    if request.method == 'POST':
-        title = request.POST.get('title')
-        print(title)
-    context = {
-    }
-
-    return render(request, "products/product_create.html", context)
